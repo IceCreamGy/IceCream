91b93f50ea4e6fd7ff8cee6d4775f50176c4105d
IceCreamGy <741446970@qq.com>
2019-04-09 20:15:39 +0800

    1.加入ABundleConfig

---
+++ ResourcesManager/Assets/Scripts/AssetBundle_Config.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "ABundleConfig", menuName = "CreatABundleConfig")]
+public class AssetBundle_Config : ScriptableObject
+{
+	//遍历这个文件夹下面所有的Prefab，所有的Prefab的名字不能重复，必须保证名字的唯一性
+	public List<string> PrefabPath = new List<string>();
+	public List<Prefab> DirectroyPath = new List<Prefab>();
+
+	[Serializable]
+	public struct Prefab
+	{
+		public string name;
+		public string path;
+	}
+}
---
+++ ResourcesManager/Assets/Scripts/GameStart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GameStart : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+		string path = "Assets/Arts/Data/AssetBundle_Config.asset";
+		Debug.Log(path);
+
+		AssetBundle_Config config = AssetDatabase.LoadAssetAtPath<AssetBundle_Config>(path);
+
+		for (int i = 0; i < config.PrefabPath.Count; i++)
+		{
+			Debug.Log(config.PrefabPath[i]);
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
