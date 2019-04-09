26c1551d59a653c8a70f4fb01b1369852041cec9
IceCreamGy <741446970@qq.com>
2019-04-09 22:01:07 +0800

    加入 BundleEditor

--- ResourcesManager/Assets/Scripts/AssetBundle_Config.cs
+++ ResourcesManager/Assets/Scripts/AssetBundle_Config.cs
@@ -8,10 +8,10 @@ public class AssetBundle_Config : ScriptableObject
 {
 	//遍历这个文件夹下面所有的Prefab，所有的Prefab的名字不能重复，必须保证名字的唯一性
 	public List<string> PrefabPath = new List<string>();
-	public List<Prefab> DirectroyPath = new List<Prefab>();
+	public List<DirInfo> DirectroyPath = new List<DirInfo>();
 
 	[Serializable]
-	public struct Prefab
+	public struct DirInfo   //目录信息
 	{
 		public string name;
 		public string path;
---
+++ ResourcesManager/Assets/Scripts/Editor/BundleEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class BundleEditor : MonoBehaviour
+{
+	static string path = "Assets/Arts/Data/AssetBundle_Config.asset";
+
+	[MenuItem("Tools/BuildABundle")]
+	private static void Build()
+	{
+		AssetBundle_Config config = AssetDatabase.LoadAssetAtPath<AssetBundle_Config>(path);
+
+		for (int i = 0; i < config.DirectroyPath.Count; i++)
+		{
+			Debug.Log(config.DirectroyPath[i].path);
+		}
+	}
+}
