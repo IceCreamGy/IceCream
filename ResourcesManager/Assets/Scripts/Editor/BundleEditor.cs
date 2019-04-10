521e79b8160afba9832c468b922d0aefd01288a7
IceCreamGy <741446970@qq.com>
2019-04-10 18:49:30 +0800

    1.设置AB包，并清除AB包名

--- ResourcesManager/Assets/Scripts/Editor/BundleEditor.cs
+++ ResourcesManager/Assets/Scripts/Editor/BundleEditor.cs
@@ -3,24 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-//进度，需要开始打包AB包3
+//进度，需要开始打包AB包4
 
 public class BundleEditor : MonoBehaviour
 {
-	static string path = "Assets/Arts/Data/ABundleConfig.asset";
+	static string ConfigPath = "Assets/Arts/Data/ABundleConfig.asset";
 	static Dictionary<string, string> m_AllFileDir = new Dictionary<string, string>();    //资源文件的路径
 	static List<string> m_AllFileAB = new List<string>();      //用来剔除的列表
 	static Dictionary<string, List<string>> m_AllPrefabDir = new Dictionary<string, List<string>>();    //单个Prefab的AB包
 
+	static AssetBundle_Config config;
+
 	[MenuItem("Tools/BuildABundle")]
 	private static void Build()
 	{
+		config = AssetDatabase.LoadAssetAtPath<AssetBundle_Config>(ConfigPath);
+
+		ClearData();
+		Load_Asset();
+		Load_Prefab();
+		ClearABName();
+
+		EditorUtility.ClearProgressBar();
+	}                           
+
+	static void ClearData()
+	{
 		m_AllFileDir.Clear();
-
-		//加载常规资源
-		//----------------------------------------------------------------------------------------------------
-		AssetBundle_Config config = AssetDatabase.LoadAssetAtPath<AssetBundle_Config>(path);
-
+		m_AllFileAB.Clear();
+		m_AllPrefabDir.Clear();
+	}
+	static void Load_Asset()      //加载常规资源
+	{	
 		for (int i = 0; i < config.DirectroyPath.Count; i++)
 		{
 			string DirName = config.DirectroyPath[i].name;
@@ -36,10 +50,14 @@ public class BundleEditor : MonoBehaviour
 			}
 		}
 
-		//开始冗余筛选
-		//----------------------------------------------------------------------------------------------------
-
-
+		//设置AB包
+		foreach (string name in m_AllFileDir.Keys)
+		{
+			SetABName(name, m_AllFileDir[name]);
+		}
+	}
+	static void Load_Prefab()       //加载Prefab，开始冗余筛选
+	{
 		string[] allStr = AssetDatabase.FindAssets("t:Prefab", config.m_AllPrefabPath.ToArray());
 		for (int i = 0; i < allStr.Length; i++)
 		{
@@ -53,8 +71,7 @@ public class BundleEditor : MonoBehaviour
 				List<string> allDependPath = new List<string>();
 
 				for (int j = 0; j < allDepend.Length; j++)
-				{
-					Debug.Log(allDepend[j]);
+				{		
 					if (!ContainAllFileAB(allDepend[j]) && !allDepend[j].EndsWith(".cs"))
 					{
 						m_AllFileAB.Add(allDepend[j]);
@@ -73,10 +90,40 @@ public class BundleEditor : MonoBehaviour
 			}
 		}
 
-
-		EditorUtility.ClearProgressBar();
+		foreach (string name in m_AllPrefabDir.Keys)
+		{
+			SetABName(name, m_AllPrefabDir[name]);
+		}	
+	}
+	static void ClearABName()      //清除AssetBundle Name，防止meta文件的修改
+	{
+		string[] oldABNames = AssetDatabase.GetAllAssetBundleNames();
+		for (int i = 0; i < oldABNames.Length; i++)
+		{
+			AssetDatabase.RemoveAssetBundleName(oldABNames[i], true);
+			EditorUtility.DisplayProgressBar("清除AB包名", " ABundle-Name" + oldABNames[i], i / oldABNames.Length);
+		}
 	}
 
+	static void SetABName(string name, string path)
+	{
+		AssetImporter assetImport = AssetImporter.GetAtPath(path);
+		if (assetImport == null)
+		{
+			Debug.LogError("路径错误：  " + path);
+		}
+		else
+		{
+			assetImport.assetBundleName = name;
+		}
+	}
+	static void SetABName(string name, List<string> paths)
+	{
+		for (int i = 0; i < paths.Count; i++)
+		{
+			SetABName(name, paths[i]);
+		}
+	}
 
 	static bool ContainAllFileAB(string path)
 	{
